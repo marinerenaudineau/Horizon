c644dcc7ebeaa9881c5db34fdd14488fdb41df27
JohannesSchroth <j.schroth@qmul.ac.uk>
2021-06-14 23:14:21 +0100

    u

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-npm install --save eko-js-sdk
 
-let ekoPlayer = new EkoPlayer('#myContainer', '1.0');
-ekoPlayer.load('AWLLK1', {
-    params: {
-        autoplay: false,
-        clearcheckpoints: true,
-        debug: true
-    },
-    events: ['nodestart', 'nodeend', 'playing', 'pause'],
-    cover: '#myCoverId',
-    iframeAttributes: { title: 'My Eko Player' }
-});
